85f014456bce21c059e0e2afc1dacc0951d4780b
NahuelChurgovich <nchurgovich@gmail.com>
2023-03-01 10:35:29 -0300

    Crecion de enumerados

---
+++ CodingChallenge.Data/Enums/EnumIdiomas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Data.Enums
+{
+    public enum EnumIdiomas
+    {
+        Castellano = 1,
+        Ingles = 2,
+        Italiano = 3,
+        Portugues = 4,
+        Aleman = 5
+    }
+}
---
+++ CodingChallenge.Data/Enums/EnumTipoFigura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Data.Enums
+{
+    public enum EnumTipoFigura
+    {
+        Circulo = 1,
+        Rectangulo = 2,
+        Triangulo = 3,
+        Cuadrado = 4,
+        Pentagono= 5,
+        Hexagono = 6,
+        Trapecio = 7
+    }
+}
